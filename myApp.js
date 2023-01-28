f45d8773965bea4e0217dbec28696b0a90e829a7
Thorfinnn <bastaher3@gmail.com>
2023-01-28 13:26:04 +0530

    assignment 5

--- myApp.js
+++ myApp.js
@@ -1,5 +1,17 @@
 let express = require('express');
 let app = express();
+app.set('json spaces',2);
+app.use('/public',express.static(__dirname+"/public"));
+
+app.get('/json',(req,res)=>{
+    res.json({
+        message: "Hello json"
+    });
+});
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/veiws/index.html");
+});
 
 
 
